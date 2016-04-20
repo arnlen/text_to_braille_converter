93e52f990a96316d5032744248ca57846117c361
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-20 21:35:03 +0200

    Initial commit

---
+++ lib/text_to_braille_converter.rb
@@ -0,0 +1,5 @@
+require "text_to_braille_converter/version"
+
+module TextToBrailleConverter
+  # Your code goes here...
+end
---
+++ lib/text_to_braille_converter/version.rb
@@ -0,0 +1,3 @@
+module TextToBrailleConverter
+  VERSION = "0.1.0"
+end
